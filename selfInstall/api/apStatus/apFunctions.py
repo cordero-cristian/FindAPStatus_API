d58bdee4f313db67059bee24d2425a11273cad9a
Cristian Cordero <cristian.cordero@charter.com>
2021-03-12 16:39:07 -0700

    updated AP Functions to include ruckus oui

--- selfInstall/api/apStatus/apFunctions.py
+++ selfInstall/api/apStatus/apFunctions.py
@@ -1,6 +1,16 @@
 from selfInstall.api.auth.decorators import tokenRequired
-
+import re
+from selfInstall.api.apStatus import ruckusOui
+from selfInstall import apiND
 
 @tokenRequired
 def work(mac):
+    apVendor = 'Cisco'
+    for oui in ruckusOui:
+        if re.search(mac, oui, re.IGNORECASE):
+            apVendor = 'Ruckus'
+            break
+    if apVendor == 'Ruckus':
+        resp = apiND.getAPStatus(mac)
+    
     return mac
