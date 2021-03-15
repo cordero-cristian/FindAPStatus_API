12bdfb93ec95dbd823a59e982c1b096ce44ea466
Cristian Cordero <cristian.cordero@charter.com>
2021-03-15 13:10:16 -0600

    Added Cisco Wlc Functions to ap functions

--- selfInstall/api/apStatus/apFunctions.py
+++ selfInstall/api/apStatus/apFunctions.py
@@ -1,16 +1,14 @@
 from selfInstall.api.auth.decorators import tokenRequired
 import re
-from selfInstall.api.apStatus import ruckusOui
-from selfInstall import apiND
+from selfInstall.CiscoFunctions.CiscoWlcFunctions import CiscoWlcFunctions
+# from selfInstall.api.apStatus import ruckusOui
+# from selfInstall import apiND
+
+ciscoFunctions = CiscoWlcFunctions()
+
 
 @tokenRequired
 def work(mac):
-    apVendor = 'Cisco'
-    for oui in ruckusOui:
-        if re.search(mac, oui, re.IGNORECASE):
-            apVendor = 'Ruckus'
-            break
-    if apVendor == 'Ruckus':
-        resp = apiND.getAPStatus(mac)
     
-    return mac
+    returnDict = ciscoFunctions.findCiscoAccessPoint('00:fc:ba:6a:b0:f0')
+    return returnDict
