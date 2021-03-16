a1b091de501e90d3289c56ca881fb87ead14c18b
Cristian Cordero <cristian.cordero@charter.com>
2021-03-16 14:47:00 -0600

    removed re import

--- selfInstall/api/apStatus/apFunctions.py
+++ selfInstall/api/apStatus/apFunctions.py
@@ -1,5 +1,4 @@
 from selfInstall.api.auth.decorators import tokenRequired
-import re
 from CiscoFunctions.CiscoWlcFunctions import CiscoWlcFunctions
 # from selfInstall.api.apStatus import ruckusOui
 # from selfInstall import apiND
@@ -9,6 +8,5 @@ ciscoFunctions = CiscoWlcFunctions()
 
 @tokenRequired
 def work(mac):
-    
     returnDict = ciscoFunctions.findCiscoAccessPoint(mac)
     return returnDict
