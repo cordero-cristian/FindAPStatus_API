4f8cd3240425dd24faf38ed42353f5d20b5dee05
Cristian Cordero <cristian.cordero@charter.com>
2021-03-16 14:19:36 -0600

    removed Model for AP will add as a feature update

--- selfInstall/api/apStatus/endpoints.py
+++ selfInstall/api/apStatus/endpoints.py
@@ -3,23 +3,19 @@ from http import HTTPStatus
 
 from flask_restx import Namespace, Resource
 
-from selfInstall.api.apStatus.apParser import accessPointStatusReqParser, user_model
+from selfInstall.api.apStatus.apParser import accessPointStatusReqParser
 from selfInstall.api.apStatus.apFunctions import work
 
 selfInstallNs = Namespace(name="self-install", validate=True)
-selfInstallNs.models[user_model.name] = user_model
-
-
 @selfInstallNs.route("/accessPointStatus", endpoint="access_point_status")
 class RegisterUser(Resource):
     """ Handles HTTP requests to URL: /api/v1/self-install/accessPointStatus """
 
     @selfInstallNs.doc(security="Bearer")
     @selfInstallNs.expect(accessPointStatusReqParser)
-    @selfInstallNs.response(int(HTTPStatus.OK), "Status of AP Goes Here", user_model)
+    @selfInstallNs.response(int(HTTPStatus.OK), "Status of AP Goes Here")
     @selfInstallNs.response(int(HTTPStatus.NOT_FOUND), "Access Point Could Not Be Found")
     @selfInstallNs.response(int(HTTPStatus.INTERNAL_SERVER_ERROR), "Internal server error")
-    @selfInstallNs.marshal_with(user_model)
     def get(self):
         """ Find an Access Point and return its Status """
         requestData = accessPointStatusReqParser.parse_args()
