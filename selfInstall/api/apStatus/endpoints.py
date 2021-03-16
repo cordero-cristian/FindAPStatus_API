1a2dc3cefef78d1642c06eceb717a97477e5bc75
Cristian Cordero <cristian.cordero@charter.com>
2021-03-16 14:46:10 -0600

    changed the 200 repsonse message displayed in swagger

--- selfInstall/api/apStatus/endpoints.py
+++ selfInstall/api/apStatus/endpoints.py
@@ -7,13 +7,15 @@ from selfInstall.api.apStatus.apParser import accessPointStatusReqParser
 from selfInstall.api.apStatus.apFunctions import work
 
 selfInstallNs = Namespace(name="self-install", validate=True)
+
+
 @selfInstallNs.route("/accessPointStatus", endpoint="access_point_status")
 class RegisterUser(Resource):
     """ Handles HTTP requests to URL: /api/v1/self-install/accessPointStatus """
 
     @selfInstallNs.doc(security="Bearer")
     @selfInstallNs.expect(accessPointStatusReqParser)
-    @selfInstallNs.response(int(HTTPStatus.OK), "Status of AP Goes Here")
+    @selfInstallNs.response(int(HTTPStatus.OK), "Access Point was found on either a vWlc or Vsz")
     @selfInstallNs.response(int(HTTPStatus.NOT_FOUND), "Access Point Could Not Be Found")
     @selfInstallNs.response(int(HTTPStatus.INTERNAL_SERVER_ERROR), "Internal server error")
     def get(self):
