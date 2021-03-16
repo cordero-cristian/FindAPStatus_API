c216eeb888da52d1b018de05aef766711a87800a
Cristian Cordero <cristian.cordero@charter.com>
2021-03-16 15:18:05 -0600

    corrected file from previous merge

--- selfInstall/api/apStatus/apFunctions.py
+++ selfInstall/api/apStatus/apFunctions.py
@@ -1,32 +1,26 @@
 from selfInstall.api.auth.decorators import tokenRequired
 from CiscoFunctions.CiscoWlcFunctions import CiscoWlcFunctions
-# from selfInstall.api.apStatus import ruckusOui
-# from selfInstall import apiND
+from selfInstall.api.apStatus import ruckusOui
+from selfInstall import apiND
+import re
 
 ciscoFunctions = CiscoWlcFunctions()
 
 
 @tokenRequired
-def work(mac):
+def getAccessPointStatus(mac):
 
     apVendor = 'Cisco'
-<<<<<<< HEAD
-#    for oui in RuckusOui:
-#        if re.search(oui, mac, re.IGNORECASE):
-#            apVendor = 'Ruckus'
-#            break
-#    if apVendor == 'Ruckus':
-#        resp = apiND.getAPStatus(mac)
-#        return resp
-    resp = ciscoFunctions.findCiscoAccessPoint(mac)
-    return resp
-=======
+
     for oui in ruckusOui:
         if re.search(mac, oui, re.IGNORECASE):
             apVendor = 'Ruckus'
             break
+
     if apVendor == 'Ruckus':
         resp = apiND.getAPStatus(mac)
         return resp
-    return mac
->>>>>>> RuckusFunctions
+
+    elif apVendor == 'Cisco':
+        resp = ciscoFunctions.findCiscoAccessPoint(mac)
+        return resp
