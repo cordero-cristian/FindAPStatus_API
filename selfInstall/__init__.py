53e123028b459e617ad203e339076fd563b2493c
“Cristian <cristian.cordero@charter.com>
2021-03-18 22:55:26 +0000

    set paramiko logs to Warnings

--- selfInstall/__init__.py
+++ selfInstall/__init__.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
@@ -6,7 +7,9 @@ from flask_sqlalchemy import SQLAlchemy
 from selfInstall.config import getConfig
 
 from RuckusFunctions import sdkRND_API
+
 apiND = sdkRND_API.clsSdkRndAPI()
+logging.getLogger("paramiko").setLevel(logging.WARNING)
 
 cors = CORS()
 db = SQLAlchemy()
