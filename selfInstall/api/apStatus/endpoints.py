bb2e1f7072b46e8da337037aaafca9075ba0f031
Cristian Cordero <cristian.cordero@charter.com>
2021-03-18 21:07:15 -0600

    updating more return respose codes

--- selfInstall/api/apStatus/endpoints.py
+++ selfInstall/api/apStatus/endpoints.py
@@ -15,11 +15,12 @@ class RegisterUser(Resource):
 
     @selfInstallNs.doc(security="Bearer")
     @selfInstallNs.expect(accessPointStatusReqParser)
-    @selfInstallNs.response(int(HTTPStatus.OK), "Access Point was found on either a vWlc or Vsz")
+    @selfInstallNs.response(int(HTTPStatus.OK), "FOUND_ON_CONTROLLER")
     @selfInstallNs.response(int(HTTPStatus.NOT_FOUND), "Access Point Could Not Be Found")
     @selfInstallNs.response(int(HTTPStatus.CONFLICT), "Busy WLC or Vsz/RND")
     @selfInstallNs.response(int(HTTPStatus.UNPROCESSABLE_ENTITY), "Not found on vWLC or Vsz")
     @selfInstallNs.response(int(HTTPStatus.INTERNAL_SERVER_ERROR), "Internal server error")
+    @selfInstallNs.response(int(HTTPStatus.BAD_REQUEST), "BAD_MAC_FORMAT")
     def get(self):
         """ Find an Access Point and return its Status """
         requestData = accessPointStatusReqParser.parse_args()
