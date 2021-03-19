16f7b4ae0f06070bf00498971ebe3d5d679f7587
Cristian Cordero <cristian.cordero@charter.com>
2021-03-18 21:12:31 -0600

    adding help to request parser

--- selfInstall/api/apStatus/apParser.py
+++ selfInstall/api/apStatus/apParser.py
@@ -4,5 +4,10 @@ from flask_restx.reqparse import RequestParser
 
 accessPointStatusReqParser = RequestParser(bundle_errors=True)
 accessPointStatusReqParser.add_argument(
-    name="mac", type=regex('([0-9A-Fa-f]{2}(:)){5}([0-9A-Fa-f]{2})'), location="headers", required=True, nullable=False
+    name="mac", 
+    type=regex('([0-9A-Fa-f]{2}(:)){5}([0-9A-Fa-f]{2})'), 
+    location="headers", 
+    required=True, 
+    nullable=False, 
+    help='BAD_MAC_FORMAT'
 )
