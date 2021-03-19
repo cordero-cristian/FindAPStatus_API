4479c81d928f4be1f73ca803dc761aeb8e372435
Cristian Cordero <cristian.cordero@charter.com>
2021-03-18 19:13:15 -0600

    fixed key error in loop

--- CiscoFunctions/CiscoWlcFunctions.py
+++ CiscoFunctions/CiscoWlcFunctions.py
@@ -170,9 +170,10 @@ class CiscoWlcFunctions():
             returnedObjects = executor.map(self.getAllAccessPointsFromSingleController, wlcIpList)
         listOfDataFrames = list()
         for obj in returnedObjects:
-            if obj['status_code']:
-                if obj['status_code'] == HTTPStatus.CONFLICT:
-                    continue
+            if isinstance(obj, dict):
+                if obj['status_code']:
+                    if obj['status_code'] == HTTPStatus.CONFLICT:
+                        continue
             # need to handle other exections here
             if isinstance(obj, pd.DataFrame):
                 # open everysingle DataFrame and dump the info into one single DataFrame and return it
