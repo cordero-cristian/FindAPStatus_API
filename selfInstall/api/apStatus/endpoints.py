265adde9b54d4638428fcbc9c212db49bcfeaf19
Cristian Cordero <cristian.cordero@charter.com>
2021-03-22 15:36:09 -0600

    added some definitions from documention to swagger doc

--- selfInstall/api/apStatus/endpoints.py
+++ selfInstall/api/apStatus/endpoints.py
@@ -21,9 +21,13 @@ Examine Response: 'status_text' for addtional Info
 
 Can be one of the following:
 
-**FOUND_ON_CONTROLLER**
-**NOT_IN_DIRECTOR**
-**NOT_ON_CONTROLLER**
+**FOUND_ON_CONTROLLER** - AP mac is found on one of the WNO controllers.  The response payload should be examined for further information regarding the AP’s readiness.
+
+
+**NOT_IN_DIRECTOR** - Applies only to Ruckus APs. The AP has not yet reached the Ruckus Network Director for its controller assignment.
+
+
+**NOT_ON_CONTROLLER** - AP is not found on a controller.
 """
                             )
     @selfInstallNs.response(int(HTTPStatus.BAD_REQUEST), """BAD_REQUEST: Incorrect formatting for a Access Point MAC Address
@@ -32,7 +36,7 @@ Examine Response: 'status_text' for addtional Info
 
 Can be one of the following:
 
-**BAD_MAC_FORMAT**
+**BAD_MAC_FORMAT** - This error is returned when the AP Mac is not passed to the API in the proper regex format.
     """)
     @selfInstallNs.response(int(HTTPStatus.UNAUTHORIZED), """UNAUTHORIZED: Bad JWT or Controller Logon Failure
 
@@ -40,9 +44,13 @@ Examine Response: 'status_text' for addtional Info
 
 Can be one of the following:
 
-**CONTROLLER_UNAUTHORIZED**
-**DIRECTOR_UNAUTHORIZED**
-**INVALID_TOKEN**
+**CONTROLLER_UNAUTHORIZED** - Unable to authenticate directly to a WNO controller.
+
+
+**DIRECTOR_UNAUTHORIZED** - Authorization failed to a Ruckus Network director.
+
+
+**INVALID_TOKEN** - Unathorized JWT
     """)
     @selfInstallNs.response(int(HTTPStatus.SERVICE_UNAVAILABLE), """SERVICE_UNAVAILABLE: Critical Failure on a Controller System
 
@@ -50,10 +58,16 @@ Examine Response: 'status_text' for addtional Info
 
 Can be one of the following:
 
-**DIRECTOR_FAILURE**
-**NO_RND_SESSIONS**
-**CONTROLLER_FAILURE**
-**SYSTEM_EXCEPTION**
+**DIRECTOR_FAILURE** - System error querying the Ruckus Network Directors.
+
+
+**NO_RND_SESSIONS** - No Ruckus Network Director sessions are available.
+
+
+**CONTROLLER_FAILURE** - The AP mac is not found on a controller, and one of the controllers cannot be contacted.  
+
+
+**SYSTEM_EXCEPTION** - Other execption for Controller realated issues.
     """)
     def get(self):
         """ Find an Access Point and return its Status """
